d655c7c6abc59f1042bed20ea9b7411075d1b3d5
Mounika <mouni@gmail.com>
2025-12-30 18:58:25 +0530

    Fix CORS for frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/users/register",
+        "https://job-tracker-backend-e96g.onrender.com",
         { name, email, password }
       );
 
